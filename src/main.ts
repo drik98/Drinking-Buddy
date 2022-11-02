ee43f26abf1e43a890cd630defe85989be5a75b5
Hendrik Schmitz <hendrik.schmitz@aixigo.com>
2022-11-02 20:50:01 +0100

    Add vuetify

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
+import { registerPlugins } from "@/plugins";
+import vuetify from "./plugins/vuetify";
 
 const app = createApp(App);
 
+registerPlugins(app);
+
 app.use(createPinia());
 app.use(router);
+app.use(vuetify);
 
 app.mount("#app");
